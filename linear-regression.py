621d1a0806f72351914bb59b93df695bbab623ec
Irfan <irfansyarifuddin@gmail.com>
2019-07-09 12:52:42 +0700

    update

--- linear-regression.py
+++ linear-regression.py
@@ -3,12 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("./DP_LIVE_21062019100730690.csv")
-dfx=df[:14] #Filter BEL
+
+# print(df)
+
+# print(df.head)
+
+dfx = df[:14]  # Filter BEL
 lg = LinearRegression()
-x = df[["TIME"]]
-y = df["Value"]
-lg.fit(x,y)
-predict_x = lg.predict([[2019]])
-print(predict_x)
+x = dfx[["TIME"]]
+y = dfx["Value"]
+lg.fit(x, y)
+
+
+accuracy = lg.score(x, y)
+print("Akurasi Hasil: ",accuracy*100,'%')
+# y_pred = lg.predict(x)
+# print("Akurasi", accuracy_score(y, y_pred))
+year = 2019
+index = 1
+for i in range(3):
+    print("year " + str(year) + "=")
+    predict_x = lg.predict([[year]])
+    print(predict_x)
+    year = year + index
+
+# predict_x = lg.predict([[2019]])
+# print(predict_x)
+
+
